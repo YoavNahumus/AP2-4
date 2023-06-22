26b3fa2e92ef09487da805fb3247f25b205dbf0a
Sagie Tsentsiper <tsents.sagie@gmail.com>
2023-06-22 12:01:34 +0300

    RegisterActivity and switch activity

--- app/src/main/java/com/example/ap2_4/MainActivity.java
+++ app/src/main/java/com/example/ap2_4/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.ap2_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickButton = (Button) findViewById(R.id.login_btn_id);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean sucessfulLogin = true;
+                if(sucessfulLogin){
+                    Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                    myIntent.putExtra("token","placeholder");//TODO -- after loging with server get token
+                    MainActivity.this.startActivity(myIntent);
+                }
+            }
+        });
     }
 }
