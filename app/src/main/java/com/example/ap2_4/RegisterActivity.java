c6b245e81fdd61efdb6a27d4e84974b0f73f001e
Sagie Tsentsiper <tsents.sagie@gmail.com>
2023-06-25 14:29:14 +0300

    RegisterActivity photos indent fix

--- app/src/main/java/com/example/ap2_4/RegisterActivity.java
+++ app/src/main/java/com/example/ap2_4/RegisterActivity.java
@@ -17,6 +17,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    String profilePic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,38 +46,38 @@ public class RegisterActivity extends AppCompatActivity {
                         .build());
             }
         });
+    }
 
-        private void register(View v) {
-            try {
-                URL url = new URL("http://localhost:5000/Users");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Content-Type", "application/json; utf-8");
-                conn.setRequestProperty("Accept", "application/json");
-                conn.setDoOutput(true);
-                String jsonInputString = String.format(
-                        "{" +
-                                "\"username\": \"%s\"," +
-                                "\"password\": \"%s\"," +
-                                "\"profilePic\": \"%s\"," +
-                                "\"displayName\": \"%s\"" +
-                                "}", ((EditText) findViewById(R.id.username)).getText(),
-                        ((EditText) findViewById(R.id.password)).getText(),
-                        profilePic,
-                        ((EditText) findViewById(R.id.display_name)).getText());
+    private void register(View v) {
+        try {
+            URL url = new URL("http://localhost:5000/Users");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+            String jsonInputString = String.format(
+                    "{" +
+                            "\"username\": \"%s\"," +
+                            "\"password\": \"%s\"," +
+                            "\"profilePic\": \"%s\"," +
+                            "\"displayName\": \"%s\"" +
+                            "}", ((EditText) findViewById(R.id.username)).getText(),
+                    ((EditText) findViewById(R.id.password)).getText(),
+                    profilePic,
+                    ((EditText) findViewById(R.id.display_name)).getText());
 
-                conn.getOutputStream().write(jsonInputString.getBytes());
-                if (conn.getResponseCode() == 200) {
-                    Intent myIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    RegisterActivity.this.startActivity(myIntent);
-                } else if (conn.getResponseCode() == 409) {
-                    Toast.makeText(getApplicationContext(), "Username already exists", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Error: " + conn.getResponseCode(), Toast.LENGTH_LONG).show();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+            conn.getOutputStream().write(jsonInputString.getBytes());
+            if (conn.getResponseCode() == 200) {
+                Intent myIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                RegisterActivity.this.startActivity(myIntent);
+            } else if (conn.getResponseCode() == 409) {
+                Toast.makeText(getApplicationContext(), "Username already exists", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Error: " + conn.getResponseCode(), Toast.LENGTH_LONG).show();
             }
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
